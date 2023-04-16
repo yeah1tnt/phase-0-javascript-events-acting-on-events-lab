40523bfb1b08e3b3c8858095768a8cd4b0abb753
Tong Tang <pichip22@hotmail.com>
2023-04-15 21:38:05 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = `#FF69B4`;
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace(`px`,``);
+    const left = parseInt(leftNumbers,10);
+
+    if(left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+function moveDodgerRight(){
+    const rightNumbers = dodger.style.left.replace(`px`,``);
+    const right = parseInt(rightNumbers,10);
+    
+    if(right > 0){
+        dodger.style.left = `${right+1}px`
+    }
+}
+
+document.addEventListener(`keydown`, function(e){
+    if (e.key === `ArrowLeft`){
+        moveDodgerLeft();
+    }
+    else if(e.key === `Arrowright`){
+        moveDodgerRight();
+    }
+})
